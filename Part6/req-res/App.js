1c4ceeb0e6e79c156aaf4e4ef14a0390a1352900
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-13 14:50:18 -0600

    create-server

--- Part6/req-res/App.js
+++ Part6/req-res/App.js
@@ -1,12 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("New request");
-  res.end("Hello, world!");
+  console.log("===> req (response)");
+  res.setHeader("conten-type", "application/json");
+  console.log(res.getHeaders());
+
+  res.end("Hello world");
 });
 
 const PORT = 4000;
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}...`);
+  console.log(`Server is listening on PORT ${PORT}...`);
 });
+
+/* console.log("===> req (request)");
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.headers); */
+
+/*  const server = http.createServer((req, res) => {
+    console.log("===> req (response)");
+    console.log(res.statusCode); // 200 OK
+  
+    res.end("Hello world");
+  }); */
