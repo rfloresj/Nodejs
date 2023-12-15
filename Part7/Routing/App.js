bf3ead7a31213f5f0d3afb665febbcb3d4fc0a74
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-14 19:01:53 -0600

    Routing: GET

--- Part7/Routing/App.js
+++ Part7/Routing/App.js
@@ -1,1 +1,36 @@
+const http = require("http");
+const courses = require("./courses");
 
+const server = http.createServer((req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      return handleRequestGET(req, res);
+    default:
+      console.log(`This method cannot be handled by the server: ${method}`);
+  }
+});
+
+const handleRequestGET = (req, res) => {
+  const path = req.url;
+  if (path === "/") {
+    res.statusCode = 200;
+    res.end("Welcome to my first server and API created with Node.js");
+  } else if (path === "/courses") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(courses.infoCourses));
+  } else if (path === "/courses/programming") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(courses.infoCourses.programming));
+  }
+
+  res.statusCode = 404;
+  res.end("The requested resource is not found...");
+};
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}.`);
+});
---
+++ Part7/Routing/courses.js
@@ -0,0 +1,44 @@
+let infoCourses = {
+  programming: [
+    {
+      id: 1,
+      title: "Learn Python",
+      language: "python",
+      views: 15000,
+      level: "basic",
+    },
+    {
+      id: 2,
+      title: "Intermediate Python",
+      language: "python",
+      views: 13553,
+      level: "middle",
+    },
+    {
+      id: 3,
+      title: "Learn JavaScript",
+      language: "javascript",
+      views: 102223,
+      level: "basic",
+    },
+  ],
+
+  mathematics: [
+    {
+      id: 1,
+      title: "Learn Calculation",
+      subject: "calculation",
+      views: 12427,
+      level: "basic",
+    },
+    {
+      id: 2,
+      title: "Learn Algebra",
+      subject: "algebra",
+      views: 15722,
+      level: "middle",
+    },
+  ],
+};
+
+module.exports.infoCourses = infoCourses;
