2b37850cc3e4711915d296d28ac5e86eead2624a
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-18 21:13:16 -0600

    req.on-POST-Body

--- Part7/Routing/App.js
+++ Part7/Routing/App.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const courses = require("./courses");
+const { infoCourses } = require("./courses");
 
 const server = http.createServer((req, res) => {
   const { method } = req;
@@ -9,22 +9,31 @@ const server = http.createServer((req, res) => {
       return handleRequestGET(req, res);
     case "POST":
       return handleRequestPOST(req, res);
+    case "PUT":
+      return handleRequestPUT(req, res);
+    case "DELETE":
+      return handleRequestDELETE(req, res);
     default:
-      console.log(`This method cannot be handled by the server: ${method}`);
+      res.statusCode = 501;
+      res.end(`This method cannot be handled by the server: ${method}`);
   }
 });
 
 const handleRequestGET = (req, res) => {
   const path = req.url;
+
+  // console.log(res.statusCode); // 200 OK (default)
+
   if (path === "/") {
-    res.statusCode = 200;
-    res.end("Welcome to my first server and API created with Node.js");
+    /* res.writeHead(200, { "Content-Type": "application/json" }); */
+    // res.statusCode = 200;
+    return res.end("Welcome to my first server and API created with Node.js");
   } else if (path === "/courses") {
-    res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses));
+    // res.statusCode = 200;
+    return res.end(JSON.stringify(infoCourses));
   } else if (path === "/courses/programming") {
-    res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses.programming));
+    // res.statusCode = 200;
+    return res.end(JSON.stringify(infoCourses.programming));
   }
 
   res.statusCode = 404;
@@ -33,10 +42,49 @@ const handleRequestGET = (req, res) => {
 
 const handleRequestPOST = (req, res) => {
   const path = req.url;
-  if(){
 
+  if (path === "/courses/programming") {
+    let body = "";
+
+    req.on("data", (content) => {
+      body += content.toString();
+    });
+
+    req.on("end", () => {
+      console.log(body);
+      console.log(typeof body);
+
+      //Convert to a JS Object
+      body = JSON.parse(body);
+
+      console.log(typeof body);
+
+      console.log(body.title);
+
+      res.end("Server received a POST request for /courses/programming");
+    });
+    // res.statusCode = 200;
+    //return res.end("Server received a POST request for /courses/programming");
   }
-}
+};
+
+const handleRequestPUT = (req, res) => {
+  const path = req.url;
+
+  if (path === "/courses") {
+    // res.statusCode = 200;
+    return res.end("Server received a PUT request for /courses");
+  }
+};
+
+const handleRequestDELETE = (req, res) => {
+  const path = req.url;
+
+  if (path === "/courses/programming") {
+    // res.statusCode = 200;
+    return res.end("Server received a DELETE request for /courses/programming");
+  }
+};
 
 const PORT = 4000;
 
