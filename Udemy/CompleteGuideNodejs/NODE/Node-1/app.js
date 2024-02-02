608925b76621a786fb9675c17fec9af9589eb2e4
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-01 19:15:06 -0600

    parsing-request-bodies

--- Udemy/CompleteGuideNodejs/NODE/Node-1/app.js
+++ Udemy/CompleteGuideNodejs/NODE/Node-1/app.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -9,6 +12,22 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    //res.writeHead(302, {}); // 302, status code for redirection
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
