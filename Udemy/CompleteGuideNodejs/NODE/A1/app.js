2b2c77e24c9b3a6213c15ad8065ed4ab9ef39ea5
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-02 18:58:45 -0600

    A1-solved

--- Udemy/CompleteGuideNodejs/NODE/A1/app.js
+++ Udemy/CompleteGuideNodejs/NODE/A1/app.js
@@ -2,8 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<h1>Welcome to the home server.</h1>");
+    res.write(
+      '<form action="/create-user" method="POST"><input type="text" name="message" placeholder="username"><button type="submit">Send</button></form>'
+    );
     return res.end();
   }
   if (url === "/users") {
@@ -16,9 +21,22 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  // res.setHeader("Content-type", "text/html");
-  // res.write("<h1>Hello World</h1>");
-  // console.log("listening");
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+      res.statusCode = 200;
+      res.setHeader("Location", "/create-user");
+      return res.end();
+    });
+  }
 });
 
 server.listen(3000);
