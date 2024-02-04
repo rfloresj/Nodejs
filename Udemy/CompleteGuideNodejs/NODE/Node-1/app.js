d28c28f651ac760adf5baa49c8f21900cb56870c
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-03 19:32:23 -0600

    routes-admin-shop

--- Udemy/CompleteGuideNodejs/NODE/Node-1/app.js
+++ Udemy/CompleteGuideNodejs/NODE/Node-1/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3001);
 
